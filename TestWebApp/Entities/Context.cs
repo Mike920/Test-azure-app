997c48a8fd03b62ba0fb74aca21d8f81fa9c03a2
Michał Rzepka <michalrzk@gmail.com>
2020-12-05 20:57:18 +0100

    add database and Movies CRUD

---
+++ TestWebApp/Entities/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp.Entities
+{
+    public class Context : DbContext
+    {
+
+        public Context([NotNullAttribute] DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ TestWebApp/Entities/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
